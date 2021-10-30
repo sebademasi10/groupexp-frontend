a8074f111177b1b741bad74f10dad7650437f886
Sebastian Demasi <sebademasi10@gmail.com>
2021-10-30 16:40:00 -0300

    Traigo actividades del servidor

--- src/app/components/actividad/participar/participar.component.ts
+++ src/app/components/actividad/participar/participar.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { Activity } from 'src/app/models/activity.model';
+import { ActividadService } from 'src/app/services/actividad.service';
 
 @Component({
   selector: 'app-participar',
   templateUrl: './participar.component.html',
   styleUrls: ['./participar.component.scss']
 })
-export class ParticiparComponent implements OnInit {
+export class ParticiparComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  private _activitiesSubscription: Subscription;
+  constructor(
+    private activitiesService: ActividadService
+  ) { }
 
   ngOnInit(): void {
+    this._activitiesSubscription = this.activitiesService.getAll().subscribe((activities: Activity[]) => {
+      console.log(activities);
+    })
   }
 
+  ngOnDestroy(): void {
+    this._activitiesSubscription.unsubscribe();
+  }
 }
--- src/app/services/actividad.service.ts
+++ src/app/services/actividad.service.ts
@@ -11,6 +11,10 @@ export class ActividadService {
   private readonly _controllerName = 'activities';
   constructor(private httpClient: HttpClient) { }
 
+  public getAll() {
+    return this.httpClient.get<Activity[]>(this._controllerName);
+  }
+
   public save(activity: Activity) {
     return this.httpClient.post<Activity>(`${this._controllerName}`, activity).pipe(
       map(activity => activity.title)
