e6169a0e505bf1772804b2e3d4fdece9cc2d54f5
sebademasi10 <sebademasi10@gmail.com>
2021-10-31 17:15:44 -0300

    Agrego ícono para moto

--- src/app/enums/icon-by-means.enum.ts
+++ src/app/enums/icon-by-means.enum.ts
@@ -2,5 +2,6 @@ export enum ICONBYMEANS {
     "Automóvil" = "directions_car",
     "Bicicleta" = "directions_bike",
     "A pie" = "directions_walk",
-    "Correr" = "directions_run"
+    "Correr" = "directions_run",
+    "Motocicleta" = "two_wheeler"
 }
