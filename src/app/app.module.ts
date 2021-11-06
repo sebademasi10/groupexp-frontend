c5096ad9a16767938491a6118c51dad0beb52ed7
sebademasi10 <sebademasi10@gmail.com>
2021-11-06 20:29:12 -0300

    Agrego data a card de participación

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -14,6 +14,10 @@ import { AuthModule } from './auth/auth.module';
 import { AppComponent } from './app.component';
 import { JwtHelperService, JWT_OPTIONS } from '@auth0/angular-jwt';
 import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { registerLocaleData } from '@angular/common';
+import localeEsAr from '@angular/common/locales/es-AR'
+
+registerLocaleData(localeEsAr, 'es-AR')
 
 @NgModule({
   declarations: [
@@ -31,6 +35,7 @@ import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
     AuthModule
   ],
   providers: [
+    { provide: LOCALE_ID, useValue: 'es-AR' },
     { provide: JWT_OPTIONS, useValue: JWT_OPTIONS },
     JwtHelperService,
     { provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: { appearance: 'outline' } }
